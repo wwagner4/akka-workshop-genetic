0864195ddd489186a26075196b448af60a5d9680
Christian Papauschek <christian@papauschek.at>
2013-11-25 18:04:51 +0100

    enums

---
+++ src/main/scala/clashcode/robot/Candidate.scala
@@ -0,0 +1,23 @@
+package clashcode.robot
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Christian
+ * Date: 25.11.13
+ * Time: 17:39
+ * To change this template use File | Settings | File Templates.
+ */
+
+object Cell extends Enumeration {
+  val EMPTY = Value(0)
+  val WALL = Value(1)
+  val STUFF = Value(2)
+}
+
+case class Instruction() {
+
+}
+
+class Candidate {
+
+}
