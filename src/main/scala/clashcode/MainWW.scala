a3f592e500f1e27205228ae7bf0ce1361175885b
wwagner4 <wwagner4@gmail.com>
2013-12-21 14:43:12 +0100

    removed classes not related to the GA added my own Main class

---
+++ src/main/scala/clashcode/MainWW.scala
@@ -0,0 +1,31 @@
+package clashcode
+
+import akka.actor.{ Props, ActorSystem }
+import akka.routing.{ RandomRouter, ConsistentHashingRouter, FromConfig }
+import akka.cluster.routing.{ ClusterRouterSettings, ClusterRouterConfig }
+import clashcode.robot._
+import java.io.{ FileOutputStream, ObjectOutputStream, ByteArrayOutputStream }
+
+object MainWW extends App {
+
+  val code = "55355322353532311311311354322344234441130030032230303231131131134444444444444444444444444544445344113444444444444444444444444444"
+  val ev = new Evolution(200, Some(code))
+  val start = System.currentTimeMillis
+  (0 until 30000).foreach {
+    i =>
+      {
+        ev.tick(20)
+        ev.debug()
+        save("best.txt", ev.candidates.head.code.bits)
+      }
+  }
+  val done = System.currentTimeMillis - start
+  println(done)
+
+  def save(name: String, array: Seq[Byte]) {
+    val o = new FileOutputStream("best.txt")
+    o.write(array.mkString.getBytes)
+    o.close()
+  }
+
+}
