d18d7776f870fff0ff90fb8d3fe4e0f2d01bb245
wwagner4 <wwagner4@gmail.com>
2013-12-22 21:08:05 +0100

    simplified main class

--- src/main/scala/clashcode/Main.scala
+++ src/main/scala/clashcode/Main.scala
@@ -1,81 +1,52 @@
 package clashcode
 
-import akka.actor.{Props, ActorSystem}
-import akka.routing.{RandomRouter, ConsistentHashingRouter, FromConfig}
-import akka.cluster.routing.{ClusterRouterSettings, ClusterRouterConfig}
-import clashcode.robot._
-import java.io.{FileOutputStream, ObjectOutputStream, ByteArrayOutputStream}
+import java.io.FileOutputStream
+import clashcode.robot.CandidateCode
+import clashcode.robot.Evolution
+import clashcode.robot.Situations
+import java.text.SimpleDateFormat
+import java.util.Date
 
 object Main extends App {
 
-
-
-  override def main(args: Array[String]) {
-
-    //println(Situations.indexBySituation.mkString(","))
-    //Situations.translateFromPaper()
-    //return;
-
-    // println("bytes: " + getBytes(Situations.getRandomCode).length)
-
-    //val code = "55355322353532311311311354322344234441130030032230303231131131134444444444444444444444444544445344113444444444444444444444444444"
-    val ev = new Evolution(RandomCandidates(200))
-    //println(ev.tick(10).points)
-
-    //readLine()
-    val start = System.currentTimeMillis
-    (0 until 30000).foreach {
-      i => {
+  val ts: String = createTimestamp
+  
+  val ev = new Evolution(RandomCandidates(200))
+  val start = System.currentTimeMillis
+  (0 until 300).foreach {
+    i =>
+      {
         ev.tick(20)
         ev.debug()
-        save("best.txt", ev.candidates.head.code.bits)
+        save(s"best-$ts.txt", ev.candidates.head.code.bits)
       }
-    }
-    val done = System.currentTimeMillis - start
-    println(done)
-
-    /*
-    val system = ActorSystem("cluster")
-    system.actorOf(Props(classOf[Prisoner], "PrisonerA"), "player")
-
-    val router = system.actorOf(Props.empty.withRouter(
-      ClusterRouterConfig(
-        RandomRouter(),
-        ClusterRouterSettings(totalInstances = 100, routeesPath = "/user/player", allowLocalRoutees = true, useRole = None))),
-      name = "router")
-
-    system.actorOf(Props(classOf[Investigator], router), "inv")
-
-    readLine()
-    system.shutdown()
-    */
   }
-
-  def getBytes(obj: Any) = {
-    val baos = new ByteArrayOutputStream(1024)
-    val o = new ObjectOutputStream(baos)
-    o.writeObject(obj)
-    baos.toByteArray
-  }
+  val done = System.currentTimeMillis - start
+  println(done)
 
   def save(name: String, array: Seq[Byte]) {
-    val o = new FileOutputStream("best.txt")
+    val o = new FileOutputStream(name)
     o.write(array.mkString.getBytes)
     o.close()
+  }
+  
+  def createTimestamp: String = {
+    val sdf = new SimpleDateFormat("yyMMdd-HHmmss")
+    sdf.format(new Date())
   }
 
 }
 
 trait CandidateCodeFactory {
-  
-  def createCodes: Seq[CandidateCode] 
-  
+
+  def createCodes: Seq[CandidateCode]
+
 }
 
 case class RandomCandidates(poolSize: Int) extends CandidateCodeFactory {
-  
+
   def createCodes: Seq[CandidateCode] = {
     (1 to poolSize).map(_ => Situations.getRandomCode)
-  }  
-    
+  }
+
 }
